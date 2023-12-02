3ff782ab651518c52e711cbc5ec4d53e2adf69cf
IvanR96 <irios96@hotmail.com>
2023-12-01 22:16:02 -0800

    new example js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const para = document.createElement('p')
+const link = document.querySelector('a');
 
-para.innerText = "Hello There Welcome To JS."
+link.textContent = 'Mozilla Developer Network';
+
+link.href = 'https://www.developer.mozilla.org';
+
+const sect = document.querySelector('section');
+
+const para = document.createElement('p');
+
+para.textContent = 'We Hope You Enjoyed the Ride.';
+
+sect.appendChild(para);
+
+const text = document.createTextNode(" — the premier source for web development knowledge.");
+
+const linkPara = document.querySelector('p');
+
+linkPara.appendChild(text);
+
+
+
 
 
 
